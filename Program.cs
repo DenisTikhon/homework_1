384ed61fb91d2282b062b4425e1c73ab5faeae9b
DenisT <denis@tikhonov.pp.ru>
2022-08-10 10:39:25 +0300

    add task2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int B = Convert.ToInt32(Console.ReadLine());
+{ if ( A > B) {
+Console.WriteLine(" Min {1} Max {0}", A, B);} 
+else {
+Console.WriteLine(" Min {0} Max {1}", A, B);}
+}
