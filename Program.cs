564a4c23535fed8ad9c6b3f06530811e2273ca91
DenisT <denis@tikhonov.pp.ru>
2022-08-10 12:40:13 +0300

    add bugfix

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ Console.WriteLine(" Min {0} Max {1}", A, B);}
 }
 // задача №4
 Console.WriteLine("Введите первое число");
-int F; // временная переменная
+int F; // дополнитеоьная переменная
 int C = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int D = Convert.ToInt32(Console.ReadLine());
@@ -25,7 +25,7 @@ else {Console.WriteLine("Максимальное число "+ E );}
 //Задача №8
 Console.WriteLine("Введите число");
 int num = Convert.ToInt32(Console.ReadLine());
-int i=1;
+int i=1;// дополнительная переменная
     while (i<=num) {
     if ((i % 2) == 0) {Console.Write(i+","); i++;} else {i++;}
  }
