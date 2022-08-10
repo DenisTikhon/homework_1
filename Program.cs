d5d75f266771c3da52298bc7d372b0099dfd8e4b
DenisT <denis@tikhonov.pp.ru>
2022-08-10 11:09:38 +0300

    add task4

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите первое число");
+﻿//Задача №2
+Console.WriteLine("Введите первое число");
 int A = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int B = Convert.ToInt32(Console.ReadLine());
@@ -7,3 +8,18 @@ Console.WriteLine(" Min {1} Max {0}", A, B);}
 else {
 Console.WriteLine(" Min {0} Max {1}", A, B);}
 }
+// задача №4
+Console.WriteLine("Введите первое число");
+int F; // временная переменная
+int C = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int D = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int E = Convert.ToInt32(Console.ReadLine());
+{ if (C > D) { F = C ;} 
+else {F = D;}
+}{
+if (F > E) {Console.WriteLine("Максимальное число "+ F );}
+else {Console.WriteLine("Максимальное число "+ E );}
+}
+//
